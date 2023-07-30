204c8573d57759d33edeb5ac870cf888f7bcaeb1
Erkki Sula <43129445+kzree@users.noreply.github.com>
2023-07-30 23:17:10 +0300

    1089. add typescript implementation

---
+++ typescript/easy/arrays.ts
@@ -0,0 +1,20 @@
+// 1089. Duplicate zeroes
+/**
+ Do not return anything, modify arr in-place instead.
+ */
+function duplicateZeros(arr: number[]): void {
+  const maxLength = arr.length;
+  const indexes = [];
+
+  arr.forEach((element, index) => {
+    if (element === 0) {
+      indexes.push(index);
+    }
+  });
+
+  indexes.forEach((n, idx) => {
+    arr.splice(n + 1 + idx, 0, 0);
+  })
+
+  arr.splice(maxLength, arr.length - 1);
+};
