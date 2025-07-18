f7edb8a500765d4ca86e8e43e25955c9dddb7c49
Erkki Sula <43129445+kzree@users.noreply.github.com>
2025-07-18 17:03:35 +0300

    9 add alternative C implementation

--- c_cpp/easy/palindrome.c
+++ c_cpp/easy/palindrome.c
@@ -2,7 +2,27 @@
 #include <stdio.h>
 #include <string.h>
 
+// Correct solution from google
+bool isPalindromeAlt(int x) {
+    if (x < 0 || (x % 10 == 0 && x != 0)) {
+        return false;
+    }
+
+    int reversed = 0;
+    while (x > reversed) {
+        reversed = reversed * 10 + x % 10;
+        x /= 10;
+    }
+
+    return x == reversed || x == reversed / 10;
+}
+
+// Original solution
 bool isPalindrome(int x) {
+    if (x < 0) {
+        return false;
+    }
+
     char str[33];
     sprintf(str, "%d", x);
     int length = strlen(str);
